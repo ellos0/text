a0b55fa8310bd31043fa4df368f5dc1e00451840
ellos0 <mcgibbonellott@gmail.com>
2025-11-05 15:43:23 -0800

    idk why this isnt working

--- src/ed.py
+++ src/ed.py
@@ -1,30 +1,17 @@
 import curses
-from curses import wrapper
 
-class Window:
-  def __init__(self,height,width,y,x):
-    self.height = height
-    self.width = width
-    self.y = y
-    self.x = x
-    self.win = curses.newwin(height,width,y,x)
-  def box(self):
-    self.win.box()
-  def refresh(self):
-    self.win.refresh()
-  def write(self,y,x,text):
-    self.win.addstr(y,x,text)
+from window import Window
 
 def main(stdscr):
-  stdscr.clear()
-  win = Window(10,40,2,2)
+    stdscr.clear()
+    win = Window(10, 40, 2, 2)
 
-  win.write(1,1,"hello")
-  win.box()
-  
-  stdscr.refresh()
-  win.refresh()
+    win.write(1, 1, "hello")
+    win.box()
 
-  stdscr.getch()
-  
-wrapper(main)
+    stdscr.refresh()
+    win.refresh()
+
+    stdscr.getch()
+
+curses.wrapper(main)
